a584e72915eb9425814b52f1e4b85ad9216dde39
Magarets <min01134@naver.com>
2020-09-11 06:51:07 +0900

    iphone_link_edit

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,15 +1,21 @@
 import React from "react"
-
+import * as Device from "react-device-detect"
 const Footer = () => (
-    <>
-        <footer>
-            <p style={{ fontSize: "0.792em" }} >유어슈 서울특별시 동작구 상도로 369 숭실대학교 학생회관 244호<br />Copyright 유어슈. 2009-{new Date().getFullYear()}. all rights reserved.
-             Built with {` `}
-                <a href="https://www.gatsbyjs.org">Gatsby</a>
-            </p>
-            <h3 style={{ fontSize: "0.917em" }}>yourssu.com@gmail.com</h3>
-        </footer>
-    </>
+  <>
+    <footer>
+      <p style={{ fontSize: "0.792em" }}>
+        유어슈 서울특별시 동작구 상도로 369 숭실대학교 학생회관 244호
+        <br />
+        Copyright 유어슈. 2009-{new Date().getFullYear()}. all rights reserved.
+        Built with {` `}
+        <a href="https://www.gatsbyjs.org">Gatsby</a>
+      </p>
+      <p>
+        you're using {Device.deviceType} and {Device.osName}
+      </p>
+      <h3 style={{ fontSize: "0.917em" }}>yourssu.com@gmail.com</h3>
+    </footer>
+  </>
 )
 
 export default Footer
